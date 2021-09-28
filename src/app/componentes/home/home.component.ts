4cf8b3713d6a0407b2645e6c28eca2a5cca42e60
Jesus Arath <jesus.flores.p19@gmail.com>
2021-09-27 22:11:02 -0500

    Consumimos api de Spotify

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-home',
@@ -8,8 +9,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(public http: HttpClient) {
-
+  constructor(public spotify: SpotifyService) {
+    this.spotify.getNewReleases();
    }
 
   ngOnInit() {
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpotifyService {
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
+
+  getNewReleases(){
+
+    const headers = new HttpHeaders({
+      'Authorization': "Bearer BQB_ioeTQfiti2SavDadUUo6_3LS81GSzf8rXwkGpUpmkIi1tR9zRK4QVoqaT8PBxLGvjySTmwm14qc-mLM"
+    })
+
+    this.http.get("https://api.spotify.com/v1/browse/new-releases",{headers})
+    .subscribe(data =>{
+      console.log(data);
+      
+    }) 
+  }
 }
