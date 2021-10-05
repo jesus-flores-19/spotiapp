c673e827cf2074df211c262f86a4fcb22b6aea3d
Jesus Arath <jesus.flores.p19@gmail.com>
2021-10-05 00:15:19 -0500

    Widget de spotify agregado

--- src/app/pipes/doomseguro.pipe.ts
+++ src/app/pipes/doomseguro.pipe.ts
@@ -1,12 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
 
 @Pipe({
   name: 'doomseguro'
 })
 export class DoomseguroPipe implements PipeTransform {
 
+  ruta:string;
+
+  constructor(private doomSan: DomSanitizer){
+    this.ruta = "https://open.spotify.com/embed/?uri="
+  }
+
   transform(value: any, ...args: any[]): any {
-    return null;
+    return this.doomSan.bypassSecurityTrustResourceUrl(`${this.ruta}${value}`);
   }
 
 }
